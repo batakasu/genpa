10aed5491cbcd3d56eb67704f271615f4292195b
batakasu <itsuking.tenten@gmail.com>
2026-07-26 21:38:00 +0900

    refactor<all>: 絶対パスに変更

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-fetch(rootPath + '/genpa/html/header.html')
+fetch('/genpa/html/header.html')
     .then(response => {
         if (!response.ok) {
             throw new Error('ヘッダーの読み込みに失敗しました');
@@ -12,7 +12,7 @@ fetch(rootPath + '/genpa/html/header.html')
         console.error('エラー:', error);
     });
     
-fetch(rootPath + '/genpa/html/footer.html')
+fetch('/genpa/html/footer.html')
     .then(response => {
         if (!response.ok) {
             throw new Error('フッターの読み込みに失敗しました');
